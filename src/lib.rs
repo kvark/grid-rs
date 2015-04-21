d48cbb2ebe581dc52ad1aacbdedbeecd44359583
Dzmitry Malyshau <kvarkus@gmail.com>
2015-04-21 11:26:38 -0400

    Added get_distance()

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,22 @@ pub trait Grid {
     type Direction: Copy + Debug + PartialEq;
     /// Get the center point of a cell.
     fn get_cell_center(&self, Self::Coordinate) -> Point;
+    /// Get the distance between two cell centers.
+    fn get_distance(&self, a: Self::Coordinate, b: Self::Coordinate) -> Float {
+        if a != b {
+            let pa = self.get_cell_center(a);
+            let pb = self.get_cell_center(b);
+            (pa[0]*pb[0] + pa[1]*pb[1] + pa[2]*pb[2]).sqrt()
+        }else { 0.0 }
+    }
     /// Get the closest coordinate of a point.
     fn get_coordinate(&self, &Point) -> Self::Coordinate;
     /// Get the cell neighbor in a direction.
     fn get_neighbour(&self, Self::Coordinate, Self::Direction) -> Self::Coordinate;
     /// Fold over all neighbours.
-    fn fold_neighbours<U, F: Fn(U, Self::Coordinate, Self::Direction) -> U>(&self, Self::Coordinate, F, U) -> U;
+    fn fold_neighbours<U, F: Fn(U, Self::Coordinate, Self::Direction) -> U>(&self, Self::Coordinate, U, F) -> U;
     /// Fold over all cells in a radius.
-    fn fold_in_radius<U, F: Fn(U, Self::Coordinate) -> U>(&self, Self::Coordinate, Float, F, U) -> U;
+    fn fold_in_radius<U, F: Fn(U, Self::Coordinate) -> U>(&self, Self::Coordinate, Float, U, F) -> U;
 }
 
 /// Position on the grid.
